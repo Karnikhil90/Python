a709d6f41ff3a65e1fa43d42bd10aa28b9296aa9
Nikhil Karmakar <99026264+Karnikhil90@users.noreply.github.com>
2023-11-12 22:35:13 +0530

    and a new file tution/tution
    Comman and Uncomman words

---
+++ Tution/Tutions/ComAndUnCom.py
@@ -0,0 +1,24 @@
+# getting two string as a word 
+# Then:
+    # Print the comman letter and uncomman letter
+FirstWord = input('Gimme a word not a sentence: ')
+SecondWord = input('Gimme a word not a sentence: ')
+
+FirstWord = FirstWord.strip()
+SecondWord = SecondWord.strip()
+
+ 
+# comman words 
+print("="*20,'\nThe Comman words :')
+for word in FirstWord:
+    cheak = SecondWord.find(word)
+    if cheak != -1:
+        print(word)
+
+# UnComman words
+print("="*20,'\nThe UnComman words :')
+for word in FirstWord:
+    temp = 0
+    cheak = SecondWord.find(word)
+    if cheak == -1:
+        print(word)
