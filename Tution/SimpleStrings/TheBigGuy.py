85216b5fe187cc67c67691cc637614225d26cdce
Karnikhil90 <nikhilkarmakar4020@gmail.com>
2023-11-13 13:20:36 +0530

    Manage the files

--- Tution/SimpleStrings/TheBigGuy.py
+++ Tution/SimpleStrings/TheBigGuy.py
@@ -1,37 +1,41 @@
 # Two birth date and compare who is big boy
 
+from datetime import datetime
+
 # Constant Value
-currentYear = 2023
-currentMonth = 11
-currentDay = 12
+currentYear = datetime.now().year
+currentMonth = datetime.now().month
+currentDay = datetime.now().day
 
 # input segment
 
-person1 = '21/11/2000'
+person1 = ' 21/11/2000 '
 person2 = '21/02/2006'
 
-person1Day, person1Month, person1Year = person1.split('/')
-person2Day, person2Month, person2Year = person2.split('/')
+person1Day, person1Month, person1Year = person1.strip().split('/')
+person2Day, person2Month, person2Year = person2.strip().split('/')
+        
+        # Debug
 # print(person1Day, person1Month, person1Year)
 # print(person2Day, person2Month, person2Year)
 
 # Process Segment
 if int(person1Year) != int(person2Year):
   if currentYear - int(person1Year) > currentYear - int(person2Year):
-    print(person1 + ' is big boy')
+    print(person1 + ' is BIG GUY !')
   else:
-    print(person2 + ' is big boy')
+    print(person2 + ' is BIG GUY !')
 else:
   if int(person1Month) != int(person2Month):
     if currentMonth - int(person1Month) > currentMonth - int(person2Month):
-      print(person1 + ' is big boy')
+      print(person1 + ' is s BIG GUY !')
     else:
-      print(person2 + ' is big boy')
+      print(person2 + ' is s BIG GUY !')
   else:
     if int(person1Day) != int(person2Day):
       if currentDay - int(person1Day) > currentDay - int(person2Day):
-        print(person1 + ' is big boy')
+        print(person1 + ' is BIG GUY !')
       else:
-        print(person2 + ' is big boy')
+        print(person2 + ' is BIG GUY !')
     else:
       print('Both are same')
--- Tution/SimpleStrings/palindromWORDonly.py
+++ Tution/SimpleStrings/palindromWORDonly.py
@@ -1,7 +1,8 @@
-# is palindrom
+# print the palindrom words form the sentence 
+
 sentence = 'I love my mom and dad'
 nw_sentence = sentence.split()
 for word in nw_sentence:
     if len(word) != 1:
-        if word.lower() == word[::-1].lower():
+        if word.lower() == word[::-1].lower(): # Get word and == reverce the word
             print(word)
